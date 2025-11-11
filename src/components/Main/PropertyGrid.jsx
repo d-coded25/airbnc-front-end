843f72693dbedd0ff4afd14b4abbfce9a6b54f53
Your Name <you@example.com>
2025-11-11 10:55:02 +0000

    Add property grid componenet, initial structure and content, and initial styles

--- src/components/Main/PropertyGrid.jsx
+++ src/components/Main/PropertyGrid.jsx
@@ -1,1 +1,15 @@
+import './PropertyGrid.css';
 
+function PropertyGrid() {
+  return (
+    <section className="property-grid">
+      <article>
+        <ul>
+          <li>Property Grid</li>
+        </ul>
+      </article>
+    </section>
+  );
+}
+
+export default PropertyGrid;
