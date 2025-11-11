390617fbd0245cee0558124457e6bba7256dff29
Your Name <you@example.com>
2025-11-11 10:49:02 +0000

    Add footer componenet, initial structure and content, and initial styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,1 +1,16 @@
+import './footer.css';
 
+function Footer() {
+  return (
+    <footer className="footer">
+      <ul>
+        <li>
+          <a href="https://www.github.com">Link to GitHub</a>
+        </li>
+      </ul>
+      <p>&copy; 2025 Northcoders -- Designed by **</p>
+    </footer>
+  );
+}
+
+export default Footer;
