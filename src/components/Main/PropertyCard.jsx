2466ee75272f5b8600e237be50a41a22f26b1acc
Your Name <you@example.com>
2025-11-11 11:00:19 +0000

    Add property card componenet, initial structure and content, and initial styles

--- src/components/Main/PropertyCard.jsx
+++ src/components/Main/PropertyCard.jsx
@@ -1,21 +1,12 @@
 import './PropertyCard.css';
 
-function PropertyCard(props) {
-  const { property } = props;
-  const {
-    property_name: name,
-    location,
-    price_per_night: price,
-    img,
-  } = property;
-
+function PropertyCard() {
   return (
     <li className="property-card">
       <article>
-        <img src={img} alt={name} />
-        <h2>Name: {name}</h2>
-        <h3>Location: {location}</h3>
-        <h3>£{price} per night</h3>
+        <ul>
+          <li>Property Card</li>
+        </ul>
       </article>
     </li>
   );
