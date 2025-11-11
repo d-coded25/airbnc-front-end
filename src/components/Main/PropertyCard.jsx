096fe35a7d6dfaab2e4ffdd7f77b26e4e443c777
Your Name <you@example.com>
2025-11-11 12:26:13 +0000

    Add to the property card component further structure and content

--- src/components/Main/PropertyCard.jsx
+++ src/components/Main/PropertyCard.jsx
@@ -1,12 +1,21 @@
 import './PropertyCard.css';
 
-function PropertyCard() {
+function PropertyCard(props) {
+  const { property } = props;
+  const {
+    property_name: name,
+    location,
+    price_per_night: price,
+    img,
+  } = property;
+
   return (
     <li className="property-card">
       <article>
-        <ul>
-          <li>Property Card</li>
-        </ul>
+        <img src={img} alt={name} />
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
+        <h3>£{price} per night</h3>
       </article>
     </li>
   );
