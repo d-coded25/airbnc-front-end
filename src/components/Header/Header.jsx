332b5ea61ab513009393f89e99c14344d1d275bb
Your Name <you@example.com>
2025-11-11 10:39:28 +0000

    Add header componenet, initial structure and content, and initial styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Airbnc</h1>
+      <div>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
