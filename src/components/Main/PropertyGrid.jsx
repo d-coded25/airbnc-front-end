d076e252e908c31558181b277bc26c028110b290
d-coded25 <peter.stanbank@gmail.com>
2025-11-15 17:50:39 +0000

    Add state and effect to fetch and render the properties grid on load

--- src/components/Main/PropertyGrid.jsx
+++ src/components/Main/PropertyGrid.jsx
@@ -1,20 +1,46 @@
-import './PropertyGrid.css';
+import { useState, useEffect } from 'react';
 
+import { getProperties } from '../../api';
 import PropertyCard from './PropertyCard';
 
-function PropertyGrid(props) {
-  const { properties } = props;
+import './PropertyGrid.css';
+
+function PropertyGrid() {
+  const [properties, setProperties] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasErrored, setHasErrored] = useState(null);
+
+  const fetchProperties = async () => {
+    try {
+      const { properties } = await getProperties();
+      setProperties(properties);
+      setIsLoading(false);
+    } catch (err) {
+      setHasErrored(err);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProperties();
+  }, []);
 
   const propertyCards = properties.map((property) => {
     const { property_id: id } = property;
     return <PropertyCard property={property} key={id} />;
   });
 
-  return (
-    <section className="property-grid">
-      <ul>{propertyCards}</ul>
-    </section>
-  );
+  let content;
+
+  if (isLoading) {
+    content = <h2>Loading...</h2>;
+  } else if (hasErrored) {
+    content = <h2>Something went wrong...</h2>;
+  } else {
+    content = <ul>{propertyCards}</ul>;
+  }
+
+  return <section className="property-grid">{content}</section>;
 }
 
 export default PropertyGrid;
