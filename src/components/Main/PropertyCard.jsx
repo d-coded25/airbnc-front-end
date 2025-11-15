fabc27d4cdc8630460f9c048c8b2556239bf86c3
d-coded25 <peter.stanbank@gmail.com>
2025-11-15 17:52:01 +0000

    Refactor property card h2 element

--- src/components/Main/PropertyCard.jsx
+++ src/components/Main/PropertyCard.jsx
@@ -13,7 +13,7 @@ function PropertyCard(props) {
     <li className="property-card">
       <article>
         <img src={img} alt={name} />
-        <h2>Name: {name}</h2>
+        <h2>{name}</h2>
         <h3>Location: {location}</h3>
         <h3>£{price} per night</h3>
       </article>
