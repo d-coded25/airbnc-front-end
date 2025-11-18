c8ff3cc871f740fefc7277ffaae502fad3ed4035
d-coded25 <peter.stanbank@gmail.com>
2025-11-18 10:32:24 +0000

    Add axios get properties and get property reviews requests

--- src/api.js
+++ src/api.js
@@ -1,14 +1,32 @@
 import axios from 'axios';
 
+const baseURL = 'https://airbnc-northcoders-2025.onrender.com';
+
 const getProperties = async function () {
   try {
-    const { data } = await axios.get(
-      'https://airbnc-northcoders-2025.onrender.com/api/properties'
-    );
+    const { data } = await axios.get(`${baseURL}/api/properties`);
     return data;
   } catch (err) {
     console.log(err.message);
   }
 };
 
-export { getProperties };
+const getProperty = async function (id) {
+  try {
+    const { data } = await axios.get(`${baseURL}/api/properties/${id}`);
+    return data;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const getPropertyReviews = async function (id) {
+  try {
+    const { data } = await axios.get(`${baseURL}/api/properties/${id}/reviews`);
+    return data;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export { getProperties, getProperty, getPropertyReviews };
