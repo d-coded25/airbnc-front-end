c40ca1e24ca3a2a68ecbec50394a22c31e15f813
d-coded25 <peter.stanbank@gmail.com>
2025-11-24 09:30:19 +0000

    Add further header component content and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,13 @@ function Header() {
   return (
     <header className="header">
       <h1>Airbnc</h1>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
-      </div>
+      <ul>
+        <li>
+          <Link className="properties-link" to="/">
+            Home
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 }
